0e8f0bf75c7e674717950067b4ca92eb6ea71578
BitDeficit <89550307+bitdeficit@users.noreply.github.com>
2021-09-08 15:29:22 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(
+  author="BitDeficit",
+  author_email='bitdeficit@pm.me',
+  classifiers=[
+#    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+  description="Library for Accessing Upland API",
+#  license="MIT license",
+  include_package_data=True,
+  name='upland',
+  version='0.1.0',
+  zip_safe=False,
+)
